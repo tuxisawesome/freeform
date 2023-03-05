6490add21caaefec75643f0b62de07cf355a4cf6
tuxisawesome <walter.brobson@gmail.com>
2023-03-05 13:03:51 -0500

    gogogo

--- client/install.py
+++ client/install.py
@@ -1,8 +1,11 @@
 import os
 import sys
 
-os.system("pip3 install flask")
-os.system("pip3 install requests")
+# os.system("pip3 install flask")
+# os.system("pip3 install requests")
 
+with open('./version.txt','r') as file:
+    softversion = file.read()
 
+print(softversion)
 print("FreeForm has been installed.")
--- client/main.py
+++ client/main.py
@@ -2,21 +2,15 @@
 # An object of Flask class is our WSGI application.
 from flask import Flask, render_template
 import requests
+import os
 
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
 
+softversion = 1.00
 
-url = "https://raw.githubusercontent.com/tuxisawesome/freeform/main/release/versions.txt"
 
-req = requests.get(url)
-if req.status_code in [200]:
-    softversion = req.text
-else:
-    print('Could not retrieve: %s, err: %s - status code: %s' % (url, req.text, req.status_code))
-    softversion = "< Error retreving version >"
-print(softversion)
 @app.route('/')
 def home():
 	return render_template('index.html')
@@ -30,14 +24,32 @@ def settings():
 @app.route('/settings/cupdate')
 # Check for updates
 def check_for_updates():
-    # TODO: check for update code
-	return render_template('cupdate-noupdate.html', version=softversion)
+    url = "https://raw.githubusercontent.com/tuxisawesome/freeform/main/release/versions.txt"
+
+    req = requests.get(url)
+
+    if req.status_code in [200]:
+        onlinev = req.text
+    else:
+        return render_template('error.html')
+
+    if softversion < float(url):
+	    return render_template('cupdate-isupdate.html', cversion=softversion,uversion=onlinev)
+    else:
+        return render_template('cupdate-noupdate.html', version=softversion)
+
+    
 
 
 @app.route('/settings/update')
 def update_system():
     # TODO: update system
 	return render_template('update.html')
+
+@app.route('/settings/error')
+def error():
+    # TODO: update system
+	return render_template('error.html')
 
 # main driver function
 if __name__ == '__main__':
