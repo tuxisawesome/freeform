9268f3fb323927f8a6ea6226944bfef41a6aa1c9
tuxisawesome <67063826+tuxisawesome@users.noreply.github.com>
2026-05-20 21:20:06 +0000

    Fix

--- src/root/applyUpdate.sh
+++ src/root/applyUpdate.sh
@@ -2,6 +2,7 @@
 
 killall python
 killall python3
-echo "Applying update now... stage 2"
+echo "Applying update now... stage 3"
 rm -rf systemData/*
 unzip updateFile.zip -d systemData
+echo "Update applied..."
--- src/root/update.py
+++ src/root/update.py
@@ -1,8 +1,24 @@
+import os
+import requests
+
+baseURL = "https://github.com/tuxisawesome/freeform/raw/refs/heads/main/sysupdate"
+
+url = f'{baseURL}/updateFile.zip'
+
 
 
 def main():
     # Recieved update trigger
-    while True:
-        pass
+    import time
+    print("Stage 1 == Sleeping 5 seconds to allow client to display page...")
+    time.sleep(5)
+    print("Stage 2 == Download file")
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status() # Check for HTTP errors
+        with open('updateFile.zip', 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                f.write(chunk)
+    print("Starting stage 3...")
+    os.system("bash applyUpdate.sh &")
 if __name__ == "__main__":
     main()
